3a00f1f82ccc978728f7033a9551de90b1c9c169
Paulo Amaral <81058578+ppauloces@users.noreply.github.com>
2023-11-28 22:13:22 -0300

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+        $("#link-facebook").click(function() {
+            // Redirecionar para a página do Facebook
+            window.open("https://www.facebook.com/EscolaMonteRei/?locale=pt_BR", "_blank");
+        });
+    });
