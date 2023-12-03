05d81fd2399507f5b21bbb7b275516f1a84764dc
Paulo Amaral <ppaulo.developer@gmail.com>
2023-12-03 12:52:53 -0300

    galeria

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
-        $("#link-facebook").click(function() {
+    $("#link-facebook").click(function() {
             // Redirecionar para a página do Facebook
-            window.open("https://www.facebook.com/EscolaMonteRei/?locale=pt_BR", "_blank");
-        });
+        window.open("https://www.facebook.com/EscolaMonteRei/?locale=pt_BR", "_blank");
     });
+});
+
